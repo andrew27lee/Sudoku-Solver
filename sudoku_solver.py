8ce6e186f6afc5129fe2d10f617d35be5fc7b5c8
Andrew Lee <andrew27lee@yahoo.com>
2022-03-31 02:38:47 +0900

    Update error handling

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -11,7 +11,6 @@ from src.extract_n_solve.new_img_generator import *
 from src.useful_functions import my_resize
 
 app = Flask(__name__)
-image_format = {"image/jpg", "image/jpeg", "image/png"}
 input_file_path = 'static/images/input.jpg'
 
 
@@ -21,36 +20,30 @@ def main_process(im_path):
         frame = my_resize(frame, width=param_resize_width, height=param_resize_height)
     im_grids_final, points_grids, list_transform_matrix = main_grid_detector_img(frame)
     if im_grids_final is None:
-        print('Error')
-        sys.exit(3)
+        return 'Unable to detect image.'
     grids_matrix = process_extract_digits(im_grids_final, load_model('model/my_model.h5'))
     if all(elem is None for elem in grids_matrix):
-        print('Error')
-        sys.exit(3)
+        return 'Unable to extract digits from image.'
     grids_solved = main_solve_grids(grids_matrix)
-
     if grids_solved is None:
-        print('Failed to solve')
-        sys.exit(3)
+        return 'Unable to solve puzzle.'
 
     ims_filled_grid = write_solved_grids(im_grids_final, grids_matrix, grids_solved)
     im_final = recreate_img_filled(frame, ims_filled_grid, points_grids, list_transform_matrix)
     im_final = cv2.resize(im_final, (480, 480))
-
     cv2.imwrite('static/images/' + os.path.splitext(os.path.basename(im_path))[0] + "_solved.jpg", im_final)
+    return None
 
 
 @app.route('/solution', methods=['POST'])
 def solution():
-    try:
-        input_file = request.files['file']
-    except:
-        return Response('No file selected', status=400)
- 
+    error = None
+    input_file = request.files['file']
+    if input_file.filename == '':
+        return render_template('index.html', error='No file selected.')
     input_file.save(os.path.join(app.root_path, input_file_path))
-    main_process(input_file_path)
-
-    return render_template('index.html', result=True)
+    error = main_process(input_file_path)
+    return render_template('index.html', solution='static/images/input_solved.jpg', error=error)
 
 
 @app.route('/', methods=['GET'])
